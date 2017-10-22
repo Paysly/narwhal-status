6f3cc017534ed56c525f2816a33188f7fbaa489f
Tomas de Priede <tomasdepriede@gmail.com>
2017-10-22 02:17:00 -0300

    Added cron.

---
+++ backend/src/main/java/com/narwhal/health/backend/cron/HealthCheckCronServlet.java
@@ -0,0 +1,35 @@
+package com.narwhal.health.backend.cron;
+
+import com.narwhal.basics.core.rest.guice.Cron;
+import com.narwhal.basics.core.rest.guice.RelativePath;
+import com.narwhal.basics.core.rest.utils.ToStringUtils;
+import com.narwhal.health.backend.dto.HealthCheckDTO;
+import com.narwhal.health.backend.services.HealthCheckService;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Singleton
+@Cron
+@RelativePath("/health/check")
+public class HealthCheckCronServlet extends HttpServlet {
+    @Inject
+    private Logger logger;
+    @Inject
+    private HealthCheckService healthCheckService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.log(Level.INFO, "Pinging server status");
+        HealthCheckDTO healthCheckDTO = healthCheckService.pingServers();
+        //
+        logger.log(Level.INFO, "Server status: " + ToStringUtils.toString(healthCheckDTO));
+    }
+}
--- backend/src/main/java/com/narwhal/health/backend/guice/BackendModule.java
+++ backend/src/main/java/com/narwhal/health/backend/guice/BackendModule.java
@@ -2,11 +2,21 @@ package com.narwhal.health.backend.guice;
 
 import com.narwhal.basics.core.rest.guice.SubModule;
 import com.narwhal.health.backend.api.HealthCheckApi;
+import com.narwhal.health.backend.cron.HealthCheckCronServlet;
 
+import javax.servlet.http.HttpServlet;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BackendModule extends SubModule {
+    @Override
+    public List<Class<? extends HttpServlet>> cronClasses() {
+        List<Class<? extends HttpServlet>> list = new ArrayList<>();
+        //
+        list.add(HealthCheckCronServlet.class);
+        //
+        return list;
+    }
 
     @Override
     public List<Class<?>> apiClasses() {
